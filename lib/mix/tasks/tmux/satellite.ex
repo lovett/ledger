d164f267183d50ee8b2eef6d1c15d1e52871b8c4
Bill Lovett <bill@ilovett.com>
2025-02-10 20:37:14 -0500

    Add mix tasks for tmux workspace management.

---
+++ lib/mix/tasks/tmux/satellite.ex
@@ -0,0 +1,26 @@
+defmodule Mix.Tasks.Tmux.Satellite do
+  @shortdoc "Connect to a secondary tmux session"
+
+  @moduledoc """
+  The satellite workspace is for passive second-screen use.
+
+  It links the server window from the primary workspace, which must already exist.
+
+  There is no direct interaction with tmux. Commands are instead written to stdout due
+  to complications with shell integration.
+
+  This task is meant to be wrapped by a separate utility, not used directly.
+  """
+
+  use Mix.Task
+
+  @satellite "ledger-satellite"
+  @primary "ledger"
+
+  @impl Mix.Task
+  def run(_args) do
+    IO.puts("tmux new-session -d -s '#{@satellite}'")
+    IO.puts("tmux link-window -k -s '#{@primary}:server' -t '#{@satellite}:0'")
+    IO.puts("tmux attach-session -t '#{@satellite}'")
+  end
+end
---
+++ lib/mix/tasks/tmux/workspace.ex
@@ -0,0 +1,38 @@
+defmodule Mix.Tasks.Tmux.Workspace do
+  @shortdoc "Connect to a primary tmux session"
+
+  @moduledoc """
+  The primary workspace sets up a group of terminal windows for day-to-day development.
+
+  The development server is shared with the satellite workspace. See satellite.ex.
+  """
+
+  use Mix.Task
+
+  @session_name "ledger"
+
+  @impl Mix.Task
+  def run(_args) do
+    shell = System.get_env("SHELL")
+    editor = System.get_env("EDITOR")
+
+    # 0: Server
+    IO.puts("tmux new-session -d -s '#{@session_name}' -n 'server' 'mix phx.server'")
+
+    # 1: Editor
+    IO.puts("tmux new-window -a -t '#{@session_name}' '#{shell}'")
+    IO.puts("tmux send-keys -t '#{@session_name}' '#{editor} .' C-m")
+
+    # 2: Shell
+    IO.puts("tmux new-window -a -t '#{@session_name}' '#{shell}'")
+
+    # 3: iex
+    IO.puts("tmux new-window -a -t '#{@session_name}' -n 'iex' 'iex -S mix'")
+
+    # 4: Database
+    IO.puts("tmux new-window -a -t '#{@session_name}' -n 'database' 'sqlite3 ledger_dev.db'")
+
+    IO.puts("tmux select-window -t '#{@session_name}:server'")
+    IO.puts("tmux attach-session -t '#{@session_name}'")
+  end
+end
