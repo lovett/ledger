87e76a69b3f7d4dd69e2cece13371e2b614fea19
Bill Lovett <bill@ilovett.com>
2025-02-10 21:33:52 -0500

    Initial schema migration.

---
+++ priv/repo/migrations/20250210031818_initial_schema.exs
@@ -0,0 +1,60 @@
+defmodule Ledger.Repo.Migrations.InitialSchema do
+  use Ecto.Migration
+  @fts_transactions_table "transactions_fts"
+
+  def fts_up() do
+    """
+    CREATE VIRTUAL TABLE #{@fts_transactions_table} USING fts5(
+      account_id,
+      destination_id,
+      occurred_on,
+      amount,
+      payee,
+      note,
+      content='transactions',
+      content_rowid='id',
+      tokenize='porter'
+    )
+    """
+  end
+
+  def fts_down() do
+    "DROP TABLE #{@fts_transactions_table}"
+  end
+
+  def change do
+    create table(:accounts) do
+      add :name, :string
+      add :opened_on, :date
+      add :closed_on,  :date
+      add :url, :string
+      add :note, :string
+      add :logo, :binary
+      add :logo_mime, :string
+    end
+
+    create table(:transactions) do
+      add :account_id, references(:accounts, on_delete: :delete_all), null: false
+      add :destination_id, references(:acounts, on_delete: :delete_all), null: false, default: 0
+      add :occurred_on, :date
+      add :cleared_on, :date
+      add :amount, :integer
+      add :payee, :string
+      add :note, :text
+      add :receipt, :binary
+      add :receipt_mime, :string
+    end
+
+    create table(:tags) do
+      add :name, :string
+    end
+
+    create table(:transactions_tags, primary_key: false) do
+      add :transaction_id, references(:transactions), null: false, primary_key: true
+      add :tag_id, references(:tags), null: false, primary_key: true
+    end
+
+    create unique_index(:accounts, [:name])
+    execute fts_up(), fts_down()
+  end
+end
