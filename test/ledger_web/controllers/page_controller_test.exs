d006ac6689f94c8594b739796391896e0ee240db
Bill Lovett <bill@ilovett.com>
2025-02-13 16:55:42 -0500

    Update page_controller test to check for Angular-centric markup.

--- test/ledger_web/controllers/page_controller_test.exs
+++ test/ledger_web/controllers/page_controller_test.exs
@@ -1,8 +1,20 @@
 defmodule LedgerWeb.PageControllerTest do
   use LedgerWeb.ConnCase
 
+  @doc """
+  Look for the things Angular needs to run:
+    - The app root tag.
+    - The base tag.
+    - 2 JS files loaded as modules.
+    - 1 stylesheet.
+  """
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    response = html_response(conn, 200)
+    assert response =~ "<app-root></app-root>"
+    assert response =~ "<base href="
+    assert response =~ "href=\"/browser/styles.css"
+    assert response =~ "type=\"module\" src=\"/browser/polyfills.js"
+    assert response =~ "type=\"module\" src=\"/browser/main.js"
   end
 end
