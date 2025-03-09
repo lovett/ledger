4feedba489527b125f15586063e696dd2f0f8a50
Bill Lovett <bill@ilovett.com>
2025-03-09 12:10:02 -0400

    Support deletion from tag list.

--- angular/src/app/tag-list/tag-list.component.ts
+++ angular/src/app/tag-list/tag-list.component.ts
@@ -4,10 +4,11 @@ import { RouterLink } from '@angular/router';
 import { Observable, of } from 'rxjs';
 import { Tag } from '../tag';
 import { TagService } from '../tag.service';
+import { ButtonComponent } from '../button/button.component';
 
 @Component({
   selector: 'app-tag-list',
-  imports: [RouterLink, AsyncPipe, DecimalPipe],
+  imports: [RouterLink, AsyncPipe, DecimalPipe, ButtonComponent],
   templateUrl: './tag-list.component.html',
   styleUrl: './tag-list.component.css'
 })
@@ -23,10 +24,30 @@ export class TagListComponent {
     this.tags$ = this.tagService.getTags();
   }
 
+  delete(event: Event, tag: Tag) {
+    event.preventDefault();
+    const target = event.target as HTMLElement;
+    if (tag.transaction_count > 0) {
+      const confirmation = confirm(`Remove ${tag.name} from all transactions?`);
+      if (!confirmation) return;
+    }
+
+    this.tagService.deleteTag(tag.id).subscribe({
+      next: () => {
+        const node = target.closest('.tag') as HTMLElement;
+        node.parentNode!.removeChild(node);
+      },
+      error: (err: Error) => {
+        console.log(err);
+      }
+    });
+  }
+
   rename(event: Event, tag: Tag) {
     event.preventDefault();
+    const target = event.target as HTMLElement;
     const oldName = tag.name;
-    const promptResponse = prompt(`Rename ${tag.name} to:`);
+    const promptResponse = prompt(`Rename ${oldName} to:`, oldName);
 
     if (!promptResponse || promptResponse.trim() === '') {
       return;
@@ -34,6 +55,9 @@ export class TagListComponent {
 
     tag.name = promptResponse.trim();
     this.tagService.saveTag(tag).subscribe({
+      next: () => {
+        target.closest('details')?.removeAttribute('open');
+      },
       error: (err: Error) => {
         tag.name = oldName;
         console.log(err);
