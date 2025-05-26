d4ef69330a48206f5e88cfb0c35181c201598eb9
Bill Lovett <bill@ilovett.com>
2025-05-26 14:56:17 -0400

    Setup for container-based deployment.

---
+++ lib/mix/tasks/build/base.ex
@@ -0,0 +1,45 @@
+defmodule Mix.Tasks.Build.Base do
+  @shortdoc "Build the image defined by Containerfile-base"
+
+  @moduledoc """
+  The base image is a minor modification to an official Fedora image
+  that sets the default locale to UTF-8.
+
+  Out of the box, UTF-8 is not available and server startup produces
+  the following warnings:
+
+  > sh: warning: setlocale: LC_ALL: cannot change locale (en_US.UTF-8):
+  > No such file or directory
+
+  > warning: the VM is running with native name encoding of latin1 which
+  > may cause Elixir to malfunction as it expects utf8. Please ensure
+  > your locale is set to UTF-8 (which can be verified by running
+  > "locale" in your shell) or set the ELIXIR_ERL_OPTIONS="+fnu"
+  > environment variable
+
+  The base image is built separately from the application image so that
+  can happen less frequently and not drag application build time.
+
+  Once the base image has been built, the application image is built with:
+
+  ```
+  mix build.app
+  ```
+
+  """
+  use Mix.Task
+
+  def run(_args) do
+    System.cmd("podman", [
+      "build",
+      "-f",
+      "Containerfile-base",
+      "-t",
+      "ledger-base",
+      "."
+    ],
+      into: IO.stream(),
+      stderr_to_stdout: true
+    )
+  end
+end
--- lib/mix/tasks/deploy.ex
+++ lib/mix/tasks/deploy.ex
@@ -1,29 +1,70 @@
 defmodule Mix.Tasks.Deploy do
-  @shortdoc "Build the application for production"
+  @shortdoc "Publish and run the application container"
 
   @moduledoc """
-  Deployment consists of local compilation and rsync to a remote host.
+  Run this task after `mix build.app` to perform a 2-step deployment.
 
-  The local and remote host OS must match.
+  Two arguments are required:
+    - container_registry: The destination for `podman push`
+    - remote_host: The place to run `podman pull`
+
+  Step one is to get the local application image over to the
+  designated container registry.
+
+  Step two is to bring that pushed image onto the host that will run
+  it, offloading the majority of the work to Podman Quadlet and systemd.
+
+  There are 2 files not accounted for here that should be manually provisioned
+  on the container host.
+
+  The Quadlet container file (see priv/quadlet/ledger.container)
+  sources environment variables from `/etc/ledger.vars`. This file
+  should define the things that Phoenix needs to run: DATABASE_PATH,
+  PHX_HOST, PORT, and SECRET_KEY_BASE.
+
+  Use `mix phx.gen.secret` to get a value for SECRET_KEY_BASE.
+
+  DATABASE_PATH is relative to the container filesystem and should be
+  `/data/ledger.db` or similar.
+
+  The container file specifies `Image=ledger.image`. This file defines
+  the registry the application image can be pulled from. Create
+  `/etc/containers/systemd/ledger.image` and populate it with:
+
+  ```
+  [Image]
+  Image=my-container-registry.example.com/myusername/ledger:latest
+  ```
+
   """
 
   use Mix.Task
 
-  def run([host, path]) do
-    System.cmd("mix", ["assets.deploy"],
-      env: [{"MIX_ENV", "prod"}],
+  def run([container_registry, remote_host]) do
+    run(["prod", container_registry, remote_host])
+  end
+
+  def run([mix_env, container_registry, remote_host]) do
+    version = Application.spec(:ledger, :vsn)
+    |> List.to_string()
+    |> String.replace("+", ".")
+
+    # This version tag is just for reference.
+    System.cmd("podman", [
+      "push",
+      "ledger:#{mix_env}-#{version}",
+      "#{container_registry}/ledger:#{mix_env}-#{version}",
+    ],
       into: IO.stream(),
       stderr_to_stdout: true
     )
 
-    System.cmd("mix", ["compile"],
-      env: [{"MIX_ENV", "prod"}],
-      into: IO.stream(),
-      stderr_to_stdout: true
-    )
-
-    System.cmd("mix", ~w(release --overwrite),
-      env: [{"MIX_ENV", "prod"}],
+    # The latest tag is what is referenced by the Quadlet file.
+    System.cmd("podman", [
+      "push",
+      "ledger:#{mix_env}-#{version}",
+      "#{container_registry}/ledger:latest",
+    ],
       into: IO.stream(),
       stderr_to_stdout: true
     )
@@ -32,18 +73,34 @@ defmodule Mix.Tasks.Deploy do
       "rsync",
       [
         "-a",
-        "--rsync-path=sudo -u daemon rsync",
-        "--delete",
-        "_build/prod/rel/tasks/",
-        "#{host}:#{path}/app"
+        "-v",
+        "--rsync-path=sudo rsync",
+        "priv/quadlet/",
+        "#{remote_host}:/etc/containers/systemd"
       ],
       into: IO.stream(),
       stderr_to_stdout: true
     )
 
-    System.cmd("mix", ["systemd", host],
-               into: IO.stream(),
-               stderr_to_stdout: true
+    System.cmd(
+      "ssh",
+      [remote_host, "sudo systemctl daemon-reload"],
+      into: IO.stream(),
+      stderr_to_stdout: true
+    )
+
+    System.cmd(
+      "ssh",
+      [remote_host, "sudo podman pull ledger"],
+      into: IO.stream(),
+      stderr_to_stdout: true
+    )
+
+    System.cmd(
+      "ssh",
+      [remote_host, "sudo systemctl restart ledger"],
+      into: IO.stream(),
+      stderr_to_stdout: true
     )
   end
 end
