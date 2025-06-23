8649a5240c7f9481c64b56fccd62b4156101f0c1
Bill Lovett <bill@ilovett.com>
2025-06-22 21:36:39 -0400

    Set expires header on account logo.

--- lib/ledger_web/controllers/logo_controller.ex
+++ lib/ledger_web/controllers/logo_controller.ex
@@ -6,8 +6,14 @@ defmodule LedgerWeb.LogoController do
   def show(conn, %{"account_id" => account_id}) do
     data = Accounts.get_account_logo!(account_id)
     if data.logo_mime do
+      expiration = DateTime.now!("Etc/UTC")
+      |> DateTime.add(365, :day)
+      |> Calendar.strftime("%a, %d %b %Y %H:%I:00 GMT")
+
       conn
       |> put_resp_content_type(data.logo_mime)
+      |> put_resp_header("expires", expiration)
+      |> put_resp_header("cache-control", "public")
       |> send_resp(200, data.logo)
     else
       conn
