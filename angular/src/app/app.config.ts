1936d9c6ae31f7f40373aecab47d5e773f5b824a
Bill Lovett <bill@ilovett.com>
2025-08-20 20:22:37 -0400

    Resolve Angular warnings about HttpClient configuration.
    Adds withFetch() feature.

--- angular/src/app/app.config.ts
+++ angular/src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideHttpClient, withInterceptors, withFetch } from '@angular/common/http';
 
 import { cachingInterceptor } from './caching.interceptor';
 
@@ -10,6 +10,6 @@ export const appConfig: ApplicationConfig = {
     providers: [
         provideZoneChangeDetection({ eventCoalescing: true }),
         provideRouter(routes),
-        provideHttpClient(withInterceptors([cachingInterceptor])),
+        provideHttpClient(withInterceptors([cachingInterceptor]), withFetch()),
     ],
 };
