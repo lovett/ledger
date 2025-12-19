e580badd9ac7e52a1e4a57ba4534196311d468f1
Bill Lovett <bill@ilovett.com>
2025-12-19 12:07:05 -0500

    Run mix.ng within the angular directory.

--- lib/mix/tasks/angular/ng.ex
+++ lib/mix/tasks/angular/ng.ex
@@ -1,14 +1,17 @@
 defmodule Mix.Tasks.Angular.Ng do
-  @shortdoc "Run ng"
+  @shortdoc "Run ng commands"
 
   @moduledoc """
+  A wrapper for Angular's ng utility.
+
+  Commands will always be run from the angular directory.
   """
 
   use Mix.Task
 
   @impl Mix.Task
   def run(args \\ []) do
-    System.cmd("node", [ "angular/node_modules/@angular/cli/bin/ng.js" ] ++ args, into: IO.stream())
+    System.cmd("node", [ "../angular/node_modules/@angular/cli/bin/ng.js" ] ++ args, cd: "angular", into: IO.stream())
   end
 
 end
