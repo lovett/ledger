a059d947f835556b3f50c4108f2f6ec7762beb06
Bill Lovett <bill@ilovett.com>
2026-05-23 20:47:01 -0400

    Run mix format on transactions.ex.

--- lib/ledger/transactions.ex
+++ lib/ledger/transactions.ex
@@ -4,12 +4,11 @@ defmodule Ledger.Transactions do
   """
 
   import Ecto.Query, warn: false
+  alias Ledger.Accounts.Account
   alias Ledger.Repo
-
+  alias Ledger.Transactions.Fts
   alias Ledger.Transactions.Transaction
   alias Ledger.Transactions.TransactionFilter
-  alias Ledger.Transactions.Fts
-  alias Ledger.Accounts.Account
 
   @doc """
   Returns the list of transactions.
@@ -20,26 +19,37 @@ defmodule Ledger.Transactions do
       [%Transaction{}, ...]
 
   """
-  @spec list_transactions(filter:: %TransactionFilter{}) :: Ecto.Query.t()
+  @spec list_transactions(filter :: %TransactionFilter{}) :: Ecto.Query.t()
   def list_transactions(filter) do
-
     account = from a in Account, select: [:id, :name, :logo_mime]
 
-    query = from base_query(filter.tag),
-                 select: [:id, :occurred_on, :cleared_on, :amount, :payee, :note, :account_id, :destination_id, :receipt_mime],
-                 where: ^filter_account(filter),
-                 where: ^filter_account_scope(filter),
-                 where: ^filter_search(filter.search),
-                 order_by: [desc: :occurred_on],
-                 order_by: [desc: :inserted_at],
-                 limit: ^filter.limit,
-                 offset: ^filter.offset,
-                 preload: [
-                   :tags,
-                   account: ^account,
-                   destination: ^account
-                 ]
-    Repo.all query
+    query =
+      from base_query(filter.tag),
+        select: [
+          :id,
+          :occurred_on,
+          :cleared_on,
+          :amount,
+          :payee,
+          :note,
+          :account_id,
+          :destination_id,
+          :receipt_mime
+        ],
+        where: ^filter_account(filter),
+        where: ^filter_account_scope(filter),
+        where: ^filter_search(filter.search),
+        order_by: [desc: :occurred_on],
+        order_by: [desc: :inserted_at],
+        limit: ^filter.limit,
+        offset: ^filter.offset,
+        preload: [
+          :tags,
+          account: ^account,
+          destination: ^account
+        ]
+
+    Repo.all(query)
   end
 
   def base_query(""), do: Transaction
@@ -47,8 +57,6 @@ defmodule Ledger.Transactions do
   def base_query(tag) do
     from t in Transaction, join: tg in assoc(t, :tags), where: tg.name == ^tag
   end
-
-
 
   @doc """
   Returns a count of transactions.
@@ -59,13 +67,15 @@ defmodule Ledger.Transactions do
       1
 
   """
-  @spec count_transactions(filter:: %TransactionFilter{}) :: Ecto.Query.t()
+  @spec count_transactions(filter :: %TransactionFilter{}) :: Ecto.Query.t()
   def count_transactions(filter) do
-    query = from t in base_query(filter.tag),
-                 select: count(),
-                 where: ^filter_account(filter),
-                 where: ^filter_search(filter.search)
-    Repo.one query
+    query =
+      from t in base_query(filter.tag),
+        select: count(),
+        where: ^filter_account(filter),
+        where: ^filter_search(filter.search)
+
+    Repo.one(query)
   end
 
   @doc """
@@ -77,16 +87,18 @@ defmodule Ledger.Transactions do
       1
 
   """
-  @spec count_future_transactions(filter:: %TransactionFilter{}, cutoff:: Date.t()) :: Ecto.Query.t()
+  @spec count_future_transactions(filter :: %TransactionFilter{}, cutoff :: Date.t()) ::
+          Ecto.Query.t()
   def count_future_transactions(filter, cutoff) do
-    query = from t in base_query(filter.tag),
-                 select: count(),
-                 where: ^filter_account(filter),
-                 where: ^filter_search(filter.search),
-                 where: t.occurred_on > ^cutoff
-    Repo.one query
-  end
+    query =
+      from t in base_query(filter.tag),
+        select: count(),
+        where: ^filter_account(filter),
+        where: ^filter_search(filter.search),
+        where: t.occurred_on > ^cutoff
 
+    Repo.one(query)
+  end
 
   @spec filter_account(%TransactionFilter{}) :: Ecto.Query.t()
   def filter_account(filter) do
@@ -100,24 +112,38 @@ defmodule Ledger.Transactions do
   @spec filter_account_scope(%TransactionFilter{}) :: Ecto.Query.t()
   def filter_account_scope(%{account_scope: "active"}) do
     accounts = from(row in Account, select: row.id, where: is_nil(row.closed_on))
-    dynamic([t],
-            t.account_id in subquery(accounts) or is_nil(t.account_id)
-            and t.destination_id in subquery(accounts) or is_nil(t.destination_id))
+
+    dynamic(
+      [t],
+      t.account_id in subquery(accounts) or
+        (is_nil(t.account_id) and
+           t.destination_id in subquery(accounts)) or is_nil(t.destination_id)
+    )
   end
 
   def filter_account_scope(%{}) do
     dynamic(true)
   end
 
-  @spec filter_search(search:: String.t()) :: Ecto.Query.t()
+  @spec filter_search(search :: String.t()) :: Ecto.Query.t()
   def filter_search(search) do
     cond do
-      search == "" -> dynamic(true)
-      String.ends_with?(search, ":") -> dynamic(false)
-      true -> dynamic(
-                [t],
-                t.id in subquery(from(row in Fts, select: row.id, where: fragment("transactions_fts MATCH ?", ^fts_escape(search))))
-              )
+      search == "" ->
+        dynamic(true)
+
+      String.ends_with?(search, ":") ->
+        dynamic(false)
+
+      true ->
+        dynamic(
+          [t],
+          t.id in subquery(
+            from(row in Fts,
+              select: row.id,
+              where: fragment("transactions_fts MATCH ?", ^fts_escape(search))
+            )
+          )
+        )
     end
   end
 
@@ -167,13 +193,15 @@ defmodule Ledger.Transactions do
   end
 
   def balance_map(deposits, withdrawls) do
-    deposit_map = Enum.into(deposits, %{}, fn %{account_id: account_id, total_amount: total_amount} ->
-      {account_id, total_amount}
-    end)
+    deposit_map =
+      Enum.into(deposits, %{}, fn %{account_id: account_id, total_amount: total_amount} ->
+        {account_id, total_amount}
+      end)
 
-    withdrawl_map = Enum.into(withdrawls, %{}, fn %{account_id: account_id, total_amount: total_amount} ->
-      {account_id, total_amount}
-    end)
+    withdrawl_map =
+      Enum.into(withdrawls, %{}, fn %{account_id: account_id, total_amount: total_amount} ->
+        {account_id, total_amount}
+      end)
 
     Map.new(Map.keys(deposit_map) ++ Map.keys(withdrawl_map), fn key ->
       {key, Map.get(deposit_map, key, 0) - Map.get(withdrawl_map, key, 0)}
@@ -181,37 +209,45 @@ defmodule Ledger.Transactions do
   end
 
   def count_deposits() do
-    Repo.all(from t in Transaction,
-                  group_by: t.destination_id,
-                  select: %{account_id: t.destination_id, count: count()})
+    Repo.all(
+      from t in Transaction,
+        group_by: t.destination_id,
+        select: %{account_id: t.destination_id, count: count()}
+    )
     |> Enum.into(%{}, fn %{account_id: account_id, count: count} ->
-      { account_id, count }
+      {account_id, count}
     end)
   end
 
-  @spec count_deposits(account_id:: integer):: integer
+  @spec count_deposits(account_id :: integer) :: integer
   def count_deposits(account_id) do
-    Repo.one from t in Transaction,
-                  select: count(),
-                  group_by: t.destination_id,
-                  where: t.destination_id == ^account_id
+    Repo.one(
+      from t in Transaction,
+        select: count(),
+        group_by: t.destination_id,
+        where: t.destination_id == ^account_id
+    )
   end
 
   def count_withdrawls() do
-    Repo.all(from t in Transaction,
-                  group_by: t.account_id,
-                  select: %{account_id: t.account_id, count: count()})
+    Repo.all(
+      from t in Transaction,
+        group_by: t.account_id,
+        select: %{account_id: t.account_id, count: count()}
+    )
     |> Enum.into(%{}, fn %{account_id: account_id, count: count} ->
-      { account_id, count }
+      {account_id, count}
     end)
   end
 
-  @spec count_withdrawls(account_id:: integer):: integer
+  @spec count_withdrawls(account_id :: integer) :: integer
   def count_withdrawls(account_id) do
-    Repo.one from t in Transaction,
-                  select: count(),
-                  group_by: t.account_id,
-                  where: t.account_id == ^account_id
+    Repo.one(
+      from t in Transaction,
+        select: count(),
+        group_by: t.account_id,
+        where: t.account_id == ^account_id
+    )
   end
 
   def tally_deposits() do
@@ -235,11 +271,13 @@ defmodule Ledger.Transactions do
   end
 
   def tally_deposits(filter) do
-    Repo.all from t in Transaction,
-                  where: ^filter,
-                  group_by: t.destination_id,
-                  having: t.destination_id,
-                  select: %{account_id: t.destination_id, total_amount: sum(t.amount)}
+    Repo.all(
+      from t in Transaction,
+        where: ^filter,
+        group_by: t.destination_id,
+        having: t.destination_id,
+        select: %{account_id: t.destination_id, total_amount: sum(t.amount)}
+    )
   end
 
   def tally_withdrawls do
@@ -263,11 +301,13 @@ defmodule Ledger.Transactions do
   end
 
   def tally_withdrawls(filter) do
-    Repo.all from t in Transaction,
-                  where: ^filter,
-                  group_by: t.account_id,
-                  having: t.account_id,
-                  select: %{account_id: t.account_id, total_amount: sum(t.amount)}
+    Repo.all(
+      from t in Transaction,
+        where: ^filter,
+        group_by: t.account_id,
+        having: t.account_id,
+        select: %{account_id: t.account_id, total_amount: sum(t.amount)}
+    )
   end
 
   @doc """
@@ -285,31 +325,34 @@ defmodule Ledger.Transactions do
 
   """
   def get_transaction!(id) do
-    account_preload = from a in Account,
-                           select: [:id, :name, :logo_mime]
+    account_preload =
+      from a in Account,
+        select: [:id, :name, :logo_mime]
 
-    Repo.one! from t in Transaction,
-                   select: [
-                     :id,
-                     :payee,
-                     :amount,
-                     :occurred_on,
-                     :cleared_on,
-                     :account_id,
-                     :destination_id,
-                     :note,
-                     :receipt_mime
-                   ],
-                   where: t.id == ^id,
-                   preload: [:tags,
-                             account: ^account_preload,
-                             destination: ^account_preload]
+    Repo.one!(
+      from t in Transaction,
+        select: [
+          :id,
+          :payee,
+          :amount,
+          :occurred_on,
+          :cleared_on,
+          :account_id,
+          :destination_id,
+          :note,
+          :receipt_mime
+        ],
+        where: t.id == ^id,
+        preload: [:tags, account: ^account_preload, destination: ^account_preload]
+    )
   end
 
   def get_transaction_receipt!(id) do
-    Repo.one! from t in Transaction,
-                   select: [:receipt, :receipt_mime],
-                   where: t.id == ^id
+    Repo.one!(
+      from t in Transaction,
+        select: [:receipt, :receipt_mime],
+        where: t.id == ^id
+    )
   end
 
   @doc """
